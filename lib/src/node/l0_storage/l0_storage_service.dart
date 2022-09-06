bcabd663356650ff5e6970434587f4f2986cf6ca
Mike Audi <mike@mytiki.com>
2022-09-05 23:33:44 -0500

    added l0_storage_service
    fixes #88

--- lib/src/node/l0_storage/l0_storage_service.dart
+++ lib/src/node/l0_storage/l0_storage_service.dart
@@ -3,4 +3,38 @@
  * MIT license. See LICENSE file in root directory.
  */
 
-class L0StorageService {}
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:tiki_sdk_dart/src/utils/rsa/rsa.dart' as rsa;
+import 'package:uuid/uuid.dart';
+
+import '../../utils/rsa/rsa_private_key.dart';
+import 'l0_storage_model_policy_req.dart';
+import 'l0_storage_model_policy_rsp.dart';
+import 'l0_storage_repository.dart';
+
+class L0StorageService {
+  final L0StorageRepository _repository;
+  final CryptoRSAPrivateKey _privateKey;
+
+  L0StorageService(String apiId, CryptoRSAPrivateKey privateKey)
+      : _repository = L0StorageRepository(apiId),
+        _privateKey = privateKey;
+
+  Future<L0StorageModelPolicyRsp> policy() async {
+    String stringToSign = const Uuid().v4();
+    Uint8List signature =
+        rsa.sign(_privateKey, Uint8List.fromList(utf8.encode(stringToSign)));
+    L0StorageModelPolicyRsp? rsp = await _repository.policy(
+        L0StorageModelPolicyReq(
+            pubKey: _privateKey.public.encode(),
+            signature: base64Encode(signature),
+            stringToSign: stringToSign), onFailure: (rsp) {
+      throw HttpException(
+          'HTTP Error ${rsp.statusCode}: ${jsonDecode(rsp.body)}');
+    });
+    return rsp!;
+  }
+}
--- test/node/l0_storage/l0_storage_test.dart
+++ test/node/l0_storage/l0_storage_test.dart
@@ -3,56 +3,54 @@
  * MIT license. See LICENSE file in root directory.
  */
 
-import 'dart:convert';
-import 'dart:typed_data';
+import 'dart:io';
 
 import 'package:test/test.dart';
-import 'package:tiki_sdk_dart/src/node/l0_storage/l0_storage_model_policy_req.dart';
 import 'package:tiki_sdk_dart/src/node/l0_storage/l0_storage_model_policy_rsp.dart';
-import 'package:tiki_sdk_dart/src/node/l0_storage/l0_storage_repository.dart';
+import 'package:tiki_sdk_dart/src/node/l0_storage/l0_storage_service.dart';
 import 'package:tiki_sdk_dart/src/utils/rsa/rsa.dart' as rsa;
 import 'package:uuid/uuid.dart';
 
 void main() {
-  const String apiId = 'fb429703-f3ac-47d0-8d91-33850e89d81b';
+  const String apiId = '';
 
   group('l0_storage tests', skip: apiId.isEmpty, () {
     test('Get policy', () async {
       rsa.RsaKeyPair kp = rsa.generate();
-      String stringToSign = const Uuid().v4();
-      String signature = base64Encode(rsa.sign(
-          kp.privateKey, Uint8List.fromList(utf8.encode(stringToSign))));
 
-      L0StorageRepository repository = L0StorageRepository(apiId);
+      L0StorageService service = L0StorageService(apiId, kp.privateKey);
+      L0StorageModelPolicyRsp rsp = await service.policy();
 
-      L0StorageModelPolicyRsp? rsp = await repository.policy(
-          L0StorageModelPolicyReq(
-              pubKey: kp.publicKey.encode(),
-              signature: signature,
-              stringToSign: stringToSign));
-
-      expect(rsp != null, true);
-      expect(rsp?.compute?.contains('key'), true);
-      expect(rsp?.compute?.contains('file'), true);
-      expect(rsp?.compute?.contains('content-md5'), true);
-      expect(rsp?.expires?.isAfter(DateTime.now()), true);
-      expect(rsp?.keyPrefix != null, true);
-      expect(rsp?.maxBytes, 1048576);
-      expect(rsp?.fields != null, true);
-      expect(rsp?.fields?.policy != null, true);
-      expect(rsp?.fields?.contentType, 'application/json');
-      expect(rsp?.fields?.xAmzCredential != null, true);
-      expect(rsp?.fields?.xAmzAlgorithm, 'AWS4-HMAC-SHA256');
-      expect(rsp?.fields?.xAmzDate != null, true);
-      expect(rsp?.fields?.xAmzSignature != null, true);
-      expect(rsp?.fields?.xAmzObjectLockMode, 'GOVERNANCE');
-      expect(rsp?.fields?.xAmzObjectLockRetainUntilDate != null, true);
-      expect(DateTime.parse(rsp!.fields!.xAmzDate!).isBefore(DateTime.now()),
-          true);
+      expect(rsp.compute?.contains('key'), true);
+      expect(rsp.compute?.contains('file'), true);
+      expect(rsp.compute?.contains('content-md5'), true);
+      expect(rsp.expires?.isAfter(DateTime.now()), true);
+      expect(rsp.keyPrefix != null, true);
+      expect(rsp.maxBytes, 1048576);
+      expect(rsp.fields != null, true);
+      expect(rsp.fields?.policy != null, true);
+      expect(rsp.fields?.contentType, 'application/json');
+      expect(rsp.fields?.xAmzCredential != null, true);
+      expect(rsp.fields?.xAmzAlgorithm, 'AWS4-HMAC-SHA256');
+      expect(rsp.fields?.xAmzDate != null, true);
+      expect(rsp.fields?.xAmzSignature != null, true);
+      expect(rsp.fields?.xAmzObjectLockMode, 'GOVERNANCE');
+      expect(rsp.fields?.xAmzObjectLockRetainUntilDate != null, true);
+      expect(
+          DateTime.parse(rsp.fields!.xAmzDate!).isBefore(DateTime.now()), true);
       expect(
           DateTime.parse(rsp.fields!.xAmzObjectLockRetainUntilDate!)
               .isAfter(DateTime.now()),
           true);
+    });
+
+    test('Bad API Id', () async {
+      rsa.RsaKeyPair kp = rsa.generate();
+
+      L0StorageService service =
+          L0StorageService(const Uuid().v4(), kp.privateKey);
+
+      expect(() async => await service.policy(), throwsA(isA<HttpException>()));
     });
   });
 }
