611f479525ca8bb99bc4cf5e47d9696752543bc2
Mike Audi <mike@mytiki.com>
2022-09-05 23:44:13 -0500

    added API docs

--- lib/src/node/l0_storage/l0_storage_service.dart
+++ lib/src/node/l0_storage/l0_storage_service.dart
@@ -7,22 +7,32 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:tiki_sdk_dart/src/utils/rsa/rsa.dart' as rsa;
 import 'package:uuid/uuid.dart';
 
+import '../../utils/rsa/rsa.dart' as rsa;
 import '../../utils/rsa/rsa_private_key.dart';
 import 'l0_storage_model_policy_req.dart';
 import 'l0_storage_model_policy_rsp.dart';
 import 'l0_storage_repository.dart';
 
+// Service to interact with L0 Storage APIs
 class L0StorageService {
   final L0StorageRepository _repository;
   final CryptoRSAPrivateKey _privateKey;
 
+  /// Construct a new instance of [L0StorageService]
+  /// Requires a [apiId] provided by the
+  /// [L0 Storage Service](https://github.com/tiki/l0-storage) team.
+  /// Requires a [_privateKey] to sign all policy requests.
   L0StorageService(String apiId, CryptoRSAPrivateKey privateKey)
       : _repository = L0StorageRepository(apiId),
         _privateKey = privateKey;
 
+  /// Request a new [L0StorageModelPolicyRsp].
+  ///
+  /// If there is any HTTP response other than 200 the response
+  /// is thrown as an [HttpException]. Other http client exceptions
+  /// such as [SocketException] and [FormatException] are propagated.
   Future<L0StorageModelPolicyRsp> policy() async {
     String stringToSign = const Uuid().v4();
     Uint8List signature =
