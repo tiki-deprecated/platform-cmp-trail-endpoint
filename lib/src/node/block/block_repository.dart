fbd4dcd87c3ac99f4cf1e55e0cb46229cbcb7e32
Ricardo Gonçalves <ricardo@mytiki.com>
2022-09-06 11:13:34 -0300

    block done

--- lib/src/node/block/block_repository.dart
+++ lib/src/node/block/block_repository.dart
@@ -1,12 +1,24 @@
+import 'dart:convert';
+
 import 'package:sqlite3/sqlite3.dart';
 
 import '../../utils/page_model.dart';
-import '../../utils/utils.dart';
 import '../xchain/xchain_model.dart';
+import '../xchain/xchain_repository.dart';
 import 'block_model.dart';
 
 class BlockRepository {
-  static const table = 'blocks';
+  static const table = 'block';
+  static const xchainTable = XchainRepository.table;
+  static const collumnSeq = 'seq';
+  static const collumnId = 'id';
+  static const collumnVersion = 'version';
+  static const collumnPreviousHash = 'previous_hash';
+  static const collumnXchainAddress = 'xchain_id';
+  static const collumnTransactionRoot = 'transaction_root';
+  static const collumnTransactionCount = 'transaction_count';
+  static const collumnTimestamp = 'timestamp';
+
   final Database _db;
 
   BlockRepository(this._db) {
@@ -16,34 +28,42 @@ class BlockRepository {
   void createTable() async {
     _db.execute('''
       CREATE TABLE IF NOT EXISTS $table (
-          seq INTEGER AUTO INCREMENT,
-          id TEXT PRIMARY KEY,
-          version INTEGER NOT NULL,
-          previous_hash TEXT NOT NULL UNIQUE,
-          xchain_uri STRING,
-          transaction_root TEXT NOT NULL,
-          transaction_count INTEGER NOT NULL,
-          timestamp INTEGER NOT NULL
+        $collumnSeq INTEGER AUTO INCREMENT,
+        $collumnId TEXT PRIMARY KEY,
+        $collumnVersion INTEGER NOT NULL,
+        $collumnPreviousHash TEXT,
+        $collumnXchainAddress TEXT,
+        $collumnTransactionRoot BLOB,
+        $collumnTransactionCount INTEGER,
+        $collumnTimestamp INTEGER,
+
       );
     ''');
   }
 
   void save(BlockModel block) {
     _db.execute('''INSERT INTO $table VALUES (
-        ${block.seq}, 
-        ${uint8ListToBase64Url(block.id, nullable: false, addQuotes: true)}, 
-        ${block.version}, 
-        ${uint8ListToBase64Url(block.previousHash, nullable: false, addQuotes: true)},
-        '${block.xchainUri}', 
-        ${uint8ListToBase64Url(block.transactionRoot, nullable: false, addQuotes: true)},
-        ${block.transactionCount}, 
-        ${block.timestamp.millisecondsSinceEpoch ~/ 1000});''');
+      ${block.seq},
+      '${base64.encode([...block.id!])}',
+      ${block.version},
+      '${base64.encode([...block.previousHash])}',
+      ${block.xchain == null ? null : "'${block.xchain!.address}'"},
+      ${block.transactionRoot},
+      ${block.transactionCount},
+      ${block.timestamp}
+    );''');
   }
 
-  PageModel<BlockModel> getByChain(String uri, {int? page}) {
-    String whereStmt = 'WHERE xchain_uri = "$uri"';
+  PageModel<BlockModel> getByChain(String address, {int page = 0}) {
+    String whereStmt = 'WHERE ${XchainRepository.collumnAddress} = "$address"';
     List<BlockModel> blocks = _select(page: page, whereStmt: whereStmt);
-    return PageModel<BlockModel>(page ?? 0, blocks);
+    return PageModel<BlockModel>(page, blocks);
+  }
+
+  PageModel<BlockModel> getLocal({int page = 0}) {
+    String whereStmt = 'WHERE ${XchainRepository.collumnAddress} IS NULL';
+    List<BlockModel> blocks = _select(page: page, whereStmt: whereStmt);
+    return PageModel<BlockModel>(page, blocks);
   }
 
   BlockModel? getById(String id) {
@@ -51,15 +71,12 @@ class BlockRepository {
     return blocks.isNotEmpty ? blocks[0] : null;
   }
 
-  BlockModel? getLast(String xchainUri) {
-    String where = "WHERE xchain_uri = '$xchainUri'";
-    List<BlockModel> blocks = _select(whereStmt: where);
+  BlockModel? getLast({String? xchainIAddress}) {
+    String where = xchainIAddress == null
+        ? "WHERE $collumnXchainAddress IS NULL"
+        : "WHERE $collumnXchainAddress = '$xchainIAddress'";
+    List<BlockModel> blocks = _select(whereStmt: where, last: true);
     return blocks.isNotEmpty ? blocks.first : null;
-  }
-
-  List<BlockModel> getAfter(DateTime since, XchainModel xchain) {
-    int sinceInSeconds = since.millisecondsSinceEpoch ~/ 1000;
-    return _select(whereStmt: 'WHERE timestamp >= $sinceInSeconds');
   }
 
   List<BlockModel> _select(
@@ -67,29 +84,44 @@ class BlockRepository {
     String limit = page != null ? 'LIMIT ${page * 100},100' : '';
     ResultSet results = _db.select('''
         SELECT 
-          blocks.id as 'blocks.id',
-          blocks.version as 'blocks.version',
-          blocks.previous_hash as 'blocks.previous_hash',
-          blocks.xchain_uri as 'blocks.xchain_uri',
-          blocks.transaction_root as 'blocks.transaction_root',
-          blocks.transaction_count as 'blocks.transaction_count',
-          blocks.timestamp as 'blocks.timestamp'
-        FROM $table as blocks
+          $table.$collumnSeq as '$table.$collumnSeq',
+          $table.$collumnId as '$table.$collumnId',
+          $table.$collumnVersion as '$table.$collumnVersion',
+          $table.$collumnPreviousHash as '$table.$collumnPreviousHash',
+          $table.$collumnXchainAddress as '$table.$collumnXchainAddress',
+          $table.$collumnTransactionRoot as '$table.$collumnTransactionRoot',
+          $table.$collumnTransactionCount as '$table.$collumnTransactionCount',
+          $table.$collumnTimestamp as '$table.$collumnTimestamp',
+          $xchainTable.${XchainRepository.collumnAddress} as '$xchainTable.${XchainRepository.collumnAddress}',
+          $xchainTable.${XchainRepository.collumnPubkey} as '$xchainTable.${XchainRepository.collumnPubkey}',
+          $xchainTable.${XchainRepository.collumnLastChecked} as '$xchainTable.${XchainRepository.collumnLastChecked}'
+        FROM $table
+        LEFT JOIN ${XchainRepository.table} ON 
+        $table.$collumnXchainAddress = $xchainTable.${XchainRepository.collumnAddress}
         $whereStmt
-        ${last ? 'ORDER BY blocks.seq DESC' : ''};
+        ${last ? 'ORDER BY $table.$collumnSeq DESC' : ''};
         $limit
         ''');
     List<BlockModel> blocks = [];
     for (final Row row in results) {
       Map<String, dynamic> blockMap = {
-        'seq': row['seq'],
-        'id': base64UrlToUint8List(row['blocks.id']),
-        'version': row['blocks.version'],
-        'previous_hash': base64UrlToUint8List(row['blocks.previous_hash']),
-        'transaction_root':
-            base64UrlToUint8List(row['blocks.transaction_root']),
-        'transaction_count': row['blocks.transaction_count'],
-        'timestamp': row['blocks.timestamp']
+        collumnSeq: row['$table.$collumnSeq'],
+        collumnId: row['$table.$collumnId'],
+        collumnVersion: row['$table.$collumnVersion'],
+        collumnPreviousHash: row['$table.$collumnPreviousHash'],
+        collumnXchainAddress: row['$table.$collumnXchainAddress'] == null
+            ? null
+            : XchainModel.fromMap({
+              '$xchainTable.${XchainRepository.collumnAddress}' : 
+                row ['$xchainTable.${XchainRepository.collumnAddress}'],
+              '$xchainTable.${XchainRepository.collumnPubkey}' : 
+                row ['$xchainTable.${XchainRepository.collumnPubkey}'],
+              '$xchainTable.${XchainRepository.collumnLastChecked}' : 
+                row ['$xchainTable.${XchainRepository.collumnLastChecked}'],
+            }),
+        collumnTransactionRoot: row['$table.$collumnTransactionRoot'],
+        collumnTransactionCount: row['$table.$collumnTransactionCount'],
+        collumnTimestamp: row['$table.$collumnTimestamp'],
       };
       BlockModel block = BlockModel.fromMap(blockMap);
       blocks.add(block);
@@ -97,9 +129,8 @@ class BlockRepository {
     return blocks;
   }
 
-  //TODO what is this for? pruning?
-  Future<void> remove(BlockModel blk) async {
+  Future<void> prune(BlockModel blk) async {
     _db.execute(
-        'DELETE FROM $table WHERE block_id = "${uint8ListToBase64Url(blk.id, nullable: false, addQuotes: true)}"');
+        "DELETE FROM $table WHERE block_id = '${base64.encode([...blk.id!])}'");
   }
 }
--- lib/src/node/block/block_service.dart
+++ lib/src/node/block/block_service.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:sqlite3/sqlite3.dart';
@@ -7,58 +6,67 @@ import '../../utils/merkel_tree.dart';
 import '../../utils/page_model.dart';
 import '../../utils/utils.dart';
 import '../transaction/transaction_model.dart';
+import '../transaction/transaction_service.dart';
 import 'block_model.dart';
-import 'block_model_reponse.dart';
 import 'block_repository.dart';
 
 class BlockService {
   static const int version = 1;
   final BlockRepository _repository;
+  final TransactionService _transactionService;
 
-  BlockService(
-    Database db,
-  ) : _repository = BlockRepository(db);
+  BlockService(Database db, this._transactionService)
+      : _repository = BlockRepository(db);
 
   /// Create a new block from a list of transactions.
   ///
   /// Calculate the [MerkelTree] from [transactions] list.
   /// Calculate the [BlockModel.previousHash].
+  /// Update the [transactions] with block id and merkel proof;
   /// Backup the new block with [BackupService].
   /// Return the [BlockModelResponse] with [BlockModel] and [MerkelTree].
-  BlockModelResponse create(List<TransactionModel> transactions) {
+  BlockModel create(List<TransactionModel> transactions) {
     List<Uint8List> hashes = transactions.map((e) => e.id!).toList();
     MerkelTree merkelTree = MerkelTree.build(hashes);
     Uint8List transactionRoot = merkelTree.root!;
-    BlockModel? lastBlock = _repository.getLast(
-        'tiki://${transactions.first.address}'); //todo there should be no xchain ref for local
+    BlockModel? lastBlock = _repository.getLast();
     BlockModel block = BlockModel(
         previousHash:
             lastBlock == null ? Uint8List(1) : sha256(lastBlock.header()),
         transactionRoot: transactionRoot,
         transactionCount: transactions.length);
     block.id = sha256(block.header());
+    for (TransactionModel transaction in transactions) {
+      transaction.block = block;
+      transaction.merkelProof = merkelTree.proofs[transaction.id];
+      _transactionService.commit(transaction);
+    }
     _repository.save(block);
-    return BlockModelResponse(block, merkelTree);
+    return block;
   }
-
-  /// Load the [BlockModel] from local database by [BlockModel.id]
-  BlockModel? get(Uint8List id) => _repository.getById(base64Url.encode(id));
-
-  //todo xchainUri should be nullable & optional
-  /// Load the [BlockModel] by [XchainModel.uri]. If no xchain uri is provided
-  /// it loads from local database.
-  PageModel<BlockModel> getByChain(String xchainUri) {
-    return _repository.getByChain(xchainUri);
-  }
-
-  //todo xchainUri should be nullable & optional
-  /// Get the last block in the chain. If no chain is provided, get the last from
-  /// localchain.
-  BlockModel? getLast(String xchainUri) => _repository.getLast(xchainUri);
 
   /// Remove the [blk] from local database.
-  void prune(BlockModel blk) => _repository.remove(blk);
+  void prune(BlockModel blk) => _repository.prune(blk);
 
   /// Add [blockModel] in local database.
   void add(BlockModel blockModel) => _repository.save(blockModel);
+
+  /// Load the [BlockModel] from local database by [BlockModel.id]
+  BlockModel? get(String id) => _repository.getById(id);
+
+  /// Load the [BlockModel] by [XchainModel.address]. If no xchain uri is provided
+  /// it loads from local database.
+  PageModel<BlockModel> getByChain(String xchainAddress) {
+    return _repository.getByChain(xchainAddress);
+  }
+
+  PageModel<BlockModel> getLocal() {
+    return _repository.getLocal();
+  }
+
+  /// Get the last block in the chain. If no chain is provided, get the last from
+  /// localchain.
+  BlockModel? getLast(String xchainAddress) => 
+  _repository.getLast(xchainIAddress: xchainAddress);
+
 }
