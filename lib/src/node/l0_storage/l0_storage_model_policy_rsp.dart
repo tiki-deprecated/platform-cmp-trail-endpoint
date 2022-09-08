d54b4d939c27d7cae848a369fabac04b2589e5c3
Mike Audi <mike@mytiki.com>
2022-09-08 03:05:43 -0500

    move to raw map for fields
    move http errors down to repository layer. remove unnecessary == and hashcode

--- lib/src/node/l0_storage/l0_storage_model_policy_rsp.dart
+++ lib/src/node/l0_storage/l0_storage_model_policy_rsp.dart
@@ -3,15 +3,13 @@
  * MIT license. See LICENSE file in root directory.
  */
 
-import 'l0_storage_model_policy_rsp_fields.dart';
-
 /// A L0 Storage Policy Response Model
 class L0StorageModelPolicyRsp {
   DateTime? expires;
   String? keyPrefix;
   List<String>? compute;
   int? maxBytes;
-  L0StorageModelPolicyRspFields? fields;
+  Map<String, String>? fields;
 
   L0StorageModelPolicyRsp(
       {this.expires, this.keyPrefix, this.compute, this.maxBytes, this.fields});
@@ -26,7 +24,7 @@ class L0StorageModelPolicyRsp {
       if (map['compute'] != null) {
         compute = List.from(map['compute']);
       }
-      fields = L0StorageModelPolicyRspFields.fromMap(map['fields']);
+      fields = Map.from(map['fields']);
     }
   }
 
@@ -35,30 +33,11 @@ class L0StorageModelPolicyRsp {
         'keyPrefix': keyPrefix,
         'maxBytes': maxBytes,
         'compute': compute,
-        'fields': fields?.toMap()
+        'fields': fields
       };
 
   @override
   String toString() {
     return 'L0StorageModelPolicyRsp{expires: $expires, keyPrefix: $keyPrefix, compute: $compute, maxBytes: $maxBytes, fields: $fields}';
   }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is L0StorageModelPolicyRsp &&
-          runtimeType == other.runtimeType &&
-          expires == other.expires &&
-          keyPrefix == other.keyPrefix &&
-          compute == other.compute &&
-          maxBytes == other.maxBytes &&
-          fields == other.fields;
-
-  @override
-  int get hashCode =>
-      expires.hashCode ^
-      keyPrefix.hashCode ^
-      compute.hashCode ^
-      maxBytes.hashCode ^
-      fields.hashCode;
 }
