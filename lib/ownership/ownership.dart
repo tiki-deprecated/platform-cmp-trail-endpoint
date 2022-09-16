9ecb856cd043da001535ff546483127333c457d3
Ricardo Gonçalves <ricardo@mytiki.com>
2022-09-16 09:41:16 -0300

    Define ownership slice 	modified:   lib/node/node.dart 	new file:   lib/ownership/ownership.dart 	new file:   lib/ownership/ownership_model.dart 	new file:   lib/ownership/ownership_repository.dart 	new file:   lib/ownership/ownership_service.dart

---
+++ lib/ownership/ownership.dart
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+/// Manages the Ownership NFTs.
+/// {@category SDK}
+library ownership;
+
+export 'ownership_model.dart';
+export 'ownership_repository.dart';
+export 'ownership_service.dart';
---
+++ lib/ownership/ownership_repository.dart
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+import 'package:sqlite3/sqlite3.dart';
+
+/// {@category SDK}
+class OwnershipRepository {
+  final Database _db;
+
+  OwnershipRepository(this._db);
+}
