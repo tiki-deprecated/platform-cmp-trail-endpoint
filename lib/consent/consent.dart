cd8ebfe80a0604039e5b7358cfe252343754d11d
Ricardo Gonçalves <ricardo@mytiki.com>
2022-09-16 10:11:57 -0300

    consent slice base 	new file:   lib/consent/consent.dart 	new file:   lib/consent/consent_model.dart 	new file:   lib/consent/consent_repository.dart 	new file:   lib/consent/consent_service.dart 	modified:   lib/metadata/metadata.dart 	modified:   lib/tiki_sdk_data_type_enum.dart 	modified:   lib/tiki_sdk_destination.dart

---
+++ lib/consent/consent.dart
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+/// Manages the Consent NFTs.
+/// {@category SDK}
+library consent;
+
+export 'consent_model.dart';
+export 'consent_repository.dart';
+export 'consent_service.dart';
