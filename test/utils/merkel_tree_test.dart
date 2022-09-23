86bcd0361c0e80de10e80bbdfdeb48ee268ac1fa
Mike Audi <mike@mytiki.com>
2022-09-23 16:02:56 -0500

    removed node tests
    removed helper used in merkel_tree_test

--- test/utils/merkel_tree_test.dart
+++ test/utils/merkel_tree_test.dart
@@ -3,23 +3,27 @@
  * MIT license. See LICENSE file in root directory.
  */
 
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:pointycastle/export.dart';
+import 'package:sqlite3/sqlite3.dart';
 import 'package:test/test.dart';
-import 'package:tiki_sdk_dart/node/key/key_model.dart';
-import 'package:tiki_sdk_dart/node/key/key_service.dart';
-import 'package:tiki_sdk_dart/node/transaction/transaction_model.dart';
+import 'package:tiki_sdk_dart/node/node_service.dart';
 import 'package:tiki_sdk_dart/utils/merkel_tree.dart';
+import 'package:uuid/uuid.dart';
 
 import '../in_mem_key.dart';
-import '../node/node_test_helpers.dart';
 
 void main() {
   group('Merkel Tests', () {
     test('Build/Validate - 1 Txn - Success', () async {
       KeyModel key = await KeyService(InMemoryKey()).create();
-      TransactionModel txn = generateTransactionModel(1, key);
+      TransactionService transactionService =
+          TransactionService(sqlite3.openInMemory());
+      TransactionModel txn = transactionService.create(
+          Uint8List.fromList(utf8.encode(const Uuid().v4())), key);
+
       txn.id = Digest("SHA3-256").process(txn.serialize());
       MerkelTree merkelTree = MerkelTree.build([txn.id!]);
       Uint8List merkelRoot = merkelTree.root!;
@@ -29,11 +33,12 @@ void main() {
 
     test('Build/Validate - 10 Txn - Success', () async {
       KeyModel key = await KeyService(InMemoryKey()).create();
-      List<TransactionModel> txns = List.generate(10, (index) {
-        TransactionModel txn = generateTransactionModel(1, key);
-        txn.id = Digest("SHA3-256").process(txn.serialize());
-        return txn;
-      });
+      TransactionService transactionService =
+          TransactionService(sqlite3.openInMemory());
+      List<TransactionModel> txns = List.generate(
+          10,
+          (index) => transactionService.create(
+              Uint8List.fromList(utf8.encode(const Uuid().v4())), key));
       MerkelTree merkelTree =
           MerkelTree.build(txns.map((txn) => txn.id!).toList());
       for (int i = 0; i < txns.length; i++) {
@@ -46,11 +51,12 @@ void main() {
 
     test('Build/Validate - 100 Txn - Success', () async {
       KeyModel key = await KeyService(InMemoryKey()).create();
-      List<TransactionModel> txns = List.generate(100, (index) {
-        TransactionModel txn = generateTransactionModel(1, key);
-        txn.id = Digest("SHA3-256").process(txn.serialize());
-        return txn;
-      });
+      TransactionService transactionService =
+          TransactionService(sqlite3.openInMemory());
+      List<TransactionModel> txns = List.generate(
+          100,
+          (index) => transactionService.create(
+              Uint8List.fromList(utf8.encode(const Uuid().v4())), key));
       MerkelTree merkelTree =
           MerkelTree.build(txns.map((txn) => txn.id!).toList());
       for (int i = 0; i < txns.length; i++) {
@@ -63,11 +69,12 @@ void main() {
 
     test('Build/Validate - 1000 Txn - Success', () async {
       KeyModel key = await KeyService(InMemoryKey()).create();
-      List<TransactionModel> txns = List.generate(100, (index) {
-        TransactionModel txn = generateTransactionModel(1, key);
-        txn.id = Digest("SHA3-256").process(txn.serialize());
-        return txn;
-      });
+      TransactionService transactionService =
+          TransactionService(sqlite3.openInMemory());
+      List<TransactionModel> txns = List.generate(
+          100,
+          (index) => transactionService.create(
+              Uint8List.fromList(utf8.encode(const Uuid().v4())), key));
       MerkelTree merkelTree =
           MerkelTree.build(txns.map((txn) => txn.id!).toList());
       for (int i = 0; i < txns.length; i++) {
