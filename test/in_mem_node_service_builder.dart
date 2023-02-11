e8396d85e7f2cf6173343fe98acba5a8856b9e5b
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-02-10 23:50:26 -0300

    fix: remove unecessary imports and tests
    Changes
    modified:   test/in_mem_node_service_builder.dart
    modified:   test/node/transaction/transaction_service_test.dart
    modified:   test/utils/bytes_test.dart

--- test/in_mem_node_service_builder.dart
+++ test/in_mem_node_service_builder.dart
@@ -6,7 +6,6 @@
 import 'package:sqlite3/sqlite3.dart';
 import 'package:tiki_sdk_dart/node/backup/backup_service.dart';
 import 'package:tiki_sdk_dart/node/block/block_service.dart';
-import 'package:tiki_sdk_dart/node/key/key_model.dart';
 import 'package:tiki_sdk_dart/node/key/key_service.dart';
 import 'package:tiki_sdk_dart/node/node_service.dart';
 import 'package:tiki_sdk_dart/node/transaction/transaction_service.dart';
--- test/node/transaction/transaction_service_test.dart
+++ test/node/transaction/transaction_service_test.dart
@@ -9,7 +9,6 @@ import 'dart:typed_data';
 import 'package:sqlite3/sqlite3.dart';
 import 'package:test/test.dart';
 import 'package:tiki_sdk_dart/node/block/block_service.dart';
-import 'package:tiki_sdk_dart/node/key/key_model.dart';
 import 'package:tiki_sdk_dart/node/key/key_service.dart';
 import 'package:tiki_sdk_dart/node/transaction/transaction_service.dart';
 import 'package:tiki_sdk_dart/utils/merkel_tree.dart';
