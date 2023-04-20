f6946a8d473cbf6f540856d3e69955da106f7020
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-04-20 10:41:47 -0300

    fix: create example app
    move nock dependency to dev dependencies

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,3 +1,18 @@
-void main(List<String> arguments) {
-  print("Hello TIKI!");
+import 'package:example/in_mem.dart';
+import 'package:tiki_sdk_dart/tiki_sdk.dart';
+import 'package:uuid/uuid.dart';
+
+void main(List<String> arguments) async {
+    TikiSdk tikiSdk = await InMemBuilders.tikiSdk();
+    String ptr = const Uuid().v4();
+    TitleRecord title =
+      await tikiSdk.title(ptr, tags: [TitleTag.emailAddress()]);
+    print("Created a Title Record with id ${title.id} for PTR: $ptr");
+    LicenseRecord first = await tikiSdk.license(
+          ptr,
+          [
+            LicenseUse([LicenseUsecase.attribution()])
+          ],
+          'terms');
+    print("Created a License Record with id ${first.id} for PTR: $ptr");
 }
