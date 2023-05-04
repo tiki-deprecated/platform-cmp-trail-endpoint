a996db3e59a2ab7b54aec41fb456a2ac03ba3310
Harshit933 <harshitverma933@gmail.com>
2023-05-04 20:55:26 +0530

    fix : Integration test for the all method.
    This commit adds the integration test for the all method present in the core TikiSdk.

--- test/tiki_sdk_test.dart
+++ test/tiki_sdk_test.dart
@@ -52,5 +52,52 @@ void main() {
       LicenseRecord second = await tikiSdk.license(ptr, [], 'terms');
       expect(second.uses.length, 0);
     });
+
+    test('all - method - success', () async {
+      TikiSdk tikiSdk = await InMemBuilders.tikiSdk();
+      String ptr = const Uuid().v4();
+      String origin = 'com.myco.myapp';
+      List<TitleTag> tags = [TitleTag.contacts(), TitleTag.audio()];
+      String description = 'New Description';
+      await tikiSdk.title(ptr,
+          origin: origin, tags: tags, description: description);
+      String terms = "This is a new term";
+      List<LicenseUse> uses = [
+        LicenseUse([LicenseUsecase.aiTraining()]),
+        LicenseUse([LicenseUsecase.analytics()]),
+      ];
+      await tikiSdk.license(ptr, uses, terms,
+          origin: origin, expiry: DateTime(1));
+      await tikiSdk.license(ptr, uses, terms,
+          origin: origin, expiry: DateTime(2));
+      List<LicenseRecord> record = tikiSdk.all(ptr, origin: origin);
+
+      /// Case 1 : Retrieve all licenses for a valid title
+      expect(record.length, 2);
+      expect(record.elementAt(0).expiry, DateTime(2));
+      expect(record.elementAt(0).terms, terms);
+      expect(record.elementAt(1).expiry, DateTime(1));
+      expect(record.elementAt(1).terms, terms);
+      expect(record.elementAt(0).uses.elementAt(0).usecases.elementAt(0).value,
+          uses.elementAt(0).usecases.elementAt(0).value);
+      expect(record.elementAt(0).uses.elementAt(1).usecases.elementAt(0).value,
+          uses.elementAt(1).usecases.elementAt(0).value);
+
+      /// Case 2: Retrieve all licenses for an invalid title
+      String invalidPTR = "This is an invalid ptr";
+      List<LicenseRecord> invalidRecord =
+          tikiSdk.all(invalidPTR, origin: origin);
+      expect(invalidRecord, []);
+
+      /// Case 3 : Retrieve all licenses for a title with no licenses
+      String ptr2 = const Uuid().v4();
+      String origin2 = 'com.myco.myapp';
+      List<TitleTag> tags2 = [TitleTag.contacts(), TitleTag.audio()];
+      String description2 = 'New Description';
+      await tikiSdk.title(ptr2,
+          origin: origin2, tags: tags2, description: description2);
+      List<LicenseRecord> record2 = tikiSdk.all(ptr2, origin: origin2);
+      expect(record2, []);
+    });
   });
 }
